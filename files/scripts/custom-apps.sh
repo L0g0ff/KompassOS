a721c8c0ab1b644a5dbf9811b1b156307540bb9d
L0g0ff <thomas@wapnet.nl>
2025-01-24 17:56:14 +0100

    add keeper, citrix, rdm and prospect

--- files/scripts/custom-apps.sh
+++ files/scripts/custom-apps.sh
@@ -5,14 +5,33 @@ set -euo pipefail
 # if shit breaks i at least know where it is
 set -x
 
-# Interesting Repo (Thanks man!) https://github.com/sneexy-boi/bluebuild-custom/blob/main/files/scripts/system-wuzetka.sh
-
-
-# Your code goes here.
-echo 'Download and Install Keeper'
+# Interesting Repo (Thanks!) https://github.com/sneexy-boi/bluebuild-custom/blob/main/files/scripts/system-wuzetka.sh
 
 #
 # Keeper
 #
+echo 'Download and Install Keeper'
 curl -sL -o /tmp/keeper.rpm "https://download.keepersecurity.com/desktop_electron/Linux/repo/rpm/keeperpasswordmanager-16.11.3-1.x86_64.rpm"
 rpm-ostree install /tmp/keeper.rpm
+
+#
+# Citrix Workspace
+#
+echo 'Download and Install Citrix Client'
+curl -sL -o /tmp/citrix.rpm "https://downloads.citrix.com/23169/ICAClient-rhel-24.11.0.85-0.x86_64.rpm?__gda__=exp=1737740175~acl=/*~hmac=91f04a83f0adacdf352312e8a1f8fbab6cee2e42c2841f878f280adf5388002f"
+rpm-ostree install /tmp/citrix.rpm
+
+#
+# Remote Desktop Manager
+#
+echo 'Download and Install RDM'
+curl -sL -o /tmp/rdm.rpm "https://cdn.devolutions.net/download/Linux/RDM/2024.3.2.5/RemoteDesktopManager_2024.3.2.5_x86_64.rpm"
+rpm-ostree install /tmp/rdm.rpm
+
+
+#
+# Prospect Outlook
+#
+echo 'Download and Prospect Outlook Client'
+curl -sL -o /tmp/prospect.rpm "https://github.com/julian-alarcon/prospect-mail/releases/download/v0.5.4/prospect-mail-0.5.4.x86_64.rpm"
+rpm-ostree install /tmp/prospect.rpm
