e618e7b329c6f1c79ed028a69a2af5615fb8ec9a
L0g0ff <thomas@wapnet.nl>
2025-02-20 21:53:28 +0100

    add tilix

--- files/scripts/custom-apps.sh
+++ files/scripts/custom-apps.sh
@@ -8,46 +8,9 @@ set -x
 # Interesting Repo (Thanks!) https://github.com/sneexy-boi/bluebuild-custom/blob/main/files/scripts/system-wuzetka.sh
 
 #
-# Keeper
-#
-# echo 'Download and Install Keeper'
-# curl -sL -o /tmp/keeper.rpm "https://download.keepersecurity.com/desktop_electron/Linux/repo/rpm/keeperpasswordmanager-16.11.3-1.x86_64.rpm"
-# rpm-ostree install /tmp/keeper.rpm
-
-#
 # Some basic tools and libvirt-devel for vagrant
 #
-rpm-ostree install nmap telnet screen libvirt-devel
-
-# #
-# # Citrix Workspace from custom Repo
-# #
-# echo 'Download and Install Citrix Client'
-# curl -sL -o /tmp/citrix.rpm "https://repo1.famvoll.nl/ICAClient-rhel-24.11.0.85-0.x86_64.rpm"
-# rpm-ostree install /tmp/citrix.rpm
-
-# #
-# # Zoom native client (because unstable flatpak)
-# #
-# echo 'Download and Install Zoom Client'
-# curl -sL -o /tmp/zoom_x86_64.rpm "https://zoom.us/client/6.3.6.6315/zoom_x86_64.rpm"
-# rpm-ostree install /tmp/zoom_x86_64.rpm
-
-# #
-# # Remote Desktop Manager - USE Distrobox for better keeper support.
-# #
-# echo 'Download and Install RDM'
-# curl -sL -o /tmp/rdm.rpm "https://cdn.devolutions.net/download/Linux/RDM/2024.3.2.5/RemoteDesktopManager_2024.3.2.5_x86_64.rpm"
-# rpm-ostree install /tmp/rdm.rpm
-
-
-# #
-# # Prospect Outlook - USE PWA instead
-# #
-# echo 'Download and Prospect Outlook Client'
-# curl -sL -o /tmp/prospect.rpm "https://github.com/julian-alarcon/prospect-mail/releases/download/v0.5.4/prospect-mail-0.5.4.x86_64.rpm"
-# rpm-ostree install /tmp/prospect.rpm
-
+rpm-ostree install nmap telnet screen libvirt-devel tilix
 
 #
 # Keeper
@@ -72,26 +35,6 @@ rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-keeper
 
 rpm-ostree install keeperpasswordmanager
 
-rm /etc/yum.repos.d/keeper.repo -f
-
-
-
-# #
-# # Citrix Workspace from custom Repo
-# #
-# # Setup repo
-# cat << EOF > /etc/yum.repos.d/kompass.repo
-# [kompass-addons]
-# name=kompass-addons
-# baseurl=https://repo1.famvoll.nl
-# enabled=1
-# gpgcheck=0
-# EOF
-
-# rpm-ostree install ICAClient
-
-#rm /etc/yum.repos.d/kompass.repo -f
-
 #
 # Fix SSH priv ports
 #
@@ -101,3 +44,8 @@ echo 'net.ipv4.ip_unprivileged_port_start=0' > /etc/sysctl.d/50-unprivileged-por
 # Fix blurry wayland electron apps
 #
 echo 'ELECTRON_OZONE_PLATFORM_HINT=auto' >> /etc/environment
+
+#
+# Clean up the yum repo (updates are baked into new images)
+#
+rm /etc/yum.repos.d/keeper.repo -f
