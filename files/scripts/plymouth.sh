de9ad2fa804fa3fe6ffa0fa4e072e0c6500303de
L0g0ff <thomas@wapnet.nl>
2025-03-11 17:50:05 +0100

    add 2 plymouth themes with some original default (spinner) theme features

---
+++ files/scripts/plymouth.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -euo pipefail
+# if shit breaks i at least know where it is
+set -x
+
+# Source Repo (Thanks!) https://github.com/adi1090x/plymouth-themes/
+
+#
+# Theme is already copied (see recipe)
+# Set the theme (tech_b, in this case)
+# Rebuilt the initrd (see recipe)
+#
+#plymouth-set-default-theme -R target_2
+plymouth-set-default-theme -R tech_b
