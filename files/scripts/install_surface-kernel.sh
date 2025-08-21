2b68b72495a73641895a866ce993b76211cd0486
Thomas <thomas@wapnet.nl>
2025-08-21 22:01:36 +0200

    #48 change: add surface-control to installation script

--- files/scripts/install_surface-kernel.sh
+++ files/scripts/install_surface-kernel.sh
@@ -10,7 +10,7 @@ dnf5 versionlock list
 dnf5 config-manager \
     addrepo --from-repofile=https://pkg.surfacelinux.com/fedora/linux-surface.repo
 
-dnf5 -y install --allowerasing kernel-surface iptsd libwacom-surface kernel-surface-devel surface-secureboot
+dnf5 -y install --allowerasing kernel-surface iptsd libwacom-surface kernel-surface-devel surface-secureboot surface-control
 
 # Prevent kernel stuff from upgrading again
 dnf5 versionlock add kernel{,-core,-modules,-modules-core,-modules-extra,-tools,-tools-lib,-headers,-devel,-devel-matched}
