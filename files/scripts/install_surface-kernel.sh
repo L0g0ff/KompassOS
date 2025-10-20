b332b85d03ec54b79ba5fb46b7424ca481711a6d
L0g0ff <thomas@wapnet.nl>
2025-10-20 10:57:17 +0200

    bug: change install/remove order in surface kernel script

--- files/scripts/install_surface-kernel.sh
+++ files/scripts/install_surface-kernel.sh
@@ -9,11 +9,11 @@ dnf5 versionlock delete kernel{,-core,-modules,-modules-core,-modules-extra,-too
 dnf5 config-manager \
     addrepo --from-repofile=https://pkg.surfacelinux.com/fedora/linux-surface.repo
 
-# Install the Surface Linux kernel and related packages
-dnf5 -y install --allowerasing kernel-surface iptsd libwacom-surface kernel-surface-devel surface-secureboot surface-control
-
 # Remove the default Fedora kernel and related packages
 dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+
+# Install the Surface Linux kernel and related packages
+dnf5 -y install --allowerasing kernel-surface iptsd libwacom-surface kernel-surface-devel surface-secureboot surface-control
 
 # Prevent kernel stuff from upgrading again
 dnf5 versionlock add kernel{,-core,-modules,-modules-core,-modules-extra,-tools,-tools-lib,-headers,-devel,-devel-matched}
