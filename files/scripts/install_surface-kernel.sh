1018a9993d4a06b798b2a7b6c3224ddcd3922e32
L0g0ff <thomas@wapnet.nl>
2025-11-08 15:57:28 +0100

    feat: add linux-surface repository and update scripts for F43 surface compatibility

--- files/scripts/install_surface-kernel.sh
+++ files/scripts/install_surface-kernel.sh
@@ -6,8 +6,10 @@ set -ouex pipefail
 dnf5 versionlock delete kernel{,-core,-modules,-modules-core,-modules-extra,-tools,-tools-lib,-headers,-devel,-devel-matched}
 
 # Add the Surface Linux repo
-dnf5 config-manager \
-    addrepo --from-repofile=https://pkg.surfacelinux.com/fedora/linux-surface.repo
+# see recipe /files/dnf/surface-linux.repo
+# hardcoded the repo for F43 compatibility
+## dnf5 config-manager \
+##     addrepo --from-repofile=https://pkg.surfacelinux.com/fedora/linux-surface.repo
 
 # Install the Surface Linux kernel and related packages
 dnf5 -y install --allowerasing kernel-surface iptsd libwacom-surface kernel-surface-devel surface-secureboot surface-control kernel-surface-core
