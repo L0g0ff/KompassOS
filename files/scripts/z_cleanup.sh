4eba0a566f59da4f054c56629664a6e8e59b2202
L0g0ff <thomas@wapnet.nl>
2026-04-14 21:52:53 +0200

    fix: revert wallpaper settings because upstream changes.

--- files/scripts/z_cleanup.sh
+++ files/scripts/z_cleanup.sh
@@ -15,14 +15,3 @@ ln -sf /usr/share/backgrounds/kompassos/kompassos-wallpaper-1/contents/images/38
 ln -sf /usr/share/backgrounds/kompassos/kompassos-wallpaper-1/contents/images/3840x2160.png /usr/share/backgrounds/default-dark.jxl
 ln -sf /usr/share/backgrounds/kompassos/kompassos-wallpaper-1/contents/images/3840x2160.png /usr/share/backgrounds/f43/default/f43-01-night.jxl
 ln -sf /usr/share/backgrounds/kompassos/kompassos-wallpaper-1/contents/images/3840x2160.png /usr/share/backgrounds/f43/default/f43-01-day.jxl
-ln -sf /usr/share/backgrounds/kompassos/kompassos-wallpaper-1/contents /usr/share/wallpapers/Aurora
-
-#
-# Replace Next wallpaper with KompassOS wallpaper so SDDM and KDE lock screen use it on all monitors
-#
-ln -sf /usr/share/backgrounds/kompassos/kompassos-wallpaper-1/contents/images/3840x2160.png /usr/share/wallpapers/Next/contents/images/1440x2960.png
-ln -sf /usr/share/backgrounds/kompassos/kompassos-wallpaper-1/contents/images/3840x2160.png /usr/share/wallpapers/Next/contents/images/5120x2880.png
-ln -sf /usr/share/backgrounds/kompassos/kompassos-wallpaper-1/contents/images/3840x2160.png /usr/share/wallpapers/Next/contents/images/7680x2160.png
-ln -sf /usr/share/backgrounds/kompassos/kompassos-wallpaper-1/contents/images/3840x2160.png /usr/share/wallpapers/Next/contents/images_dark/1440x2960.png
-ln -sf /usr/share/backgrounds/kompassos/kompassos-wallpaper-1/contents/images/3840x2160.png /usr/share/wallpapers/Next/contents/images_dark/5120x2880.png
-ln -sf /usr/share/backgrounds/kompassos/kompassos-wallpaper-1/contents/images/3840x2160.png /usr/share/wallpapers/Next/contents/images_dark/7680x2160.png
